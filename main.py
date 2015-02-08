9ad31fd60ceafc5a47608e8b295322900dfd6eb0
Jack Chua <jack.hs.chua@gmail.com>
2015-02-07 20:26:10 -0800

    added endpoint for deep search result for an address

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
 import json
+import urllib
 from flask import Flask
 from sqlalchemy import *
+from flask import request
+import xml.dom.minidom as minidom
+import xml.etree.ElementTree as et
 
 app = Flask(__name__)
+_ZILLOW_KEY = 'X1-ZWz1az1bupzbbf_7ucak'
+_DEEP_SEARCH_API_BASE = 'http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=%s' % _ZILLOW_KEY
 
 #####################################################################
 # DATA INTERFACE
@@ -18,6 +24,12 @@ def execute(engine,query):
 	keys = res.keys()
 	values = res.fetchall()
 	return keys, values
+
+def add_args_to_api_call(apiBase, apiArgs={}):
+	apiCall = apiBase+'&'
+	for k,v in apiArgs.iteritems():
+		apiCall+=str(k) + '=' + str(v) + '&'
+	return apiCall[0:-1]
 
 #####################################################################
 
@@ -39,7 +51,22 @@ def land_data():
 
 @app.route("/land_main")
 def land_main():
-	return "Get land investment data."
+	try:
+		address = request.args.get('address').replace('\s','%20')
+		citystatezip = request.args.get('citystatezip')
+	except:
+		return "Not a valid API call."
+	apiCall = add_args_to_api_call(_DEEP_SEARCH_API_BASE, {
+		'citystatezip' : citystatezip,
+		'address'      : address
+		})
+	data = urllib.urlopen(apiCall)
+	data = minidom.parse(data)
+	# get relevant data
+	relevantData = {}
+	for f in ['citystatezip','address','lotSizeSqFt']:
+		relevantData[f] = data.getElementsByTagName(f)[0].firstChild.nodeValue
+	return json.dumps(relevantData)
 
 if __name__ == "__main__":
 	app.debug=True
